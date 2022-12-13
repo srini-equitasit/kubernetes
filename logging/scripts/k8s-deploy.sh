918783aa2ee6d3ddb64db5721040289fde2d4c35
srini-equitasit <srini@equitasitinc.com>
2022-12-13 18:51:59 +0530

    removed rbac

--- logging/scripts/k8s-deploy.sh
+++ logging/scripts/k8s-deploy.sh
@@ -2,7 +2,7 @@ kubectl apply -f ../elastic-search.yml
 
 kubectl apply -f ../kibana.yml
 
-kubectl apply -f ../rbac.yml
+# kubectl apply -f ../rbac.yml
 
 kubectl apply -f ../fluentd-daemonset.yaml
 
