c6cf10582219dd8f23f6f97ed6024f853c1b22af
srini-equitasit <srini@equitasitinc.com>
2022-12-14 09:35:33 +0530

    logging refactored

--- logging/scripts/k8s-deploy.sh
+++
@@ -1,9 +0,0 @@
-kubectl apply -f ../elastic-search.yml
-
-kubectl apply -f ../kibana.yml
-
-# kubectl apply -f ../rbac.yml
-
-kubectl apply -f ../fluentd-daemonset.yaml
-
-kubectl apply -f ../busybox.yaml
--- logging/scripts/k8s-un-deploy.sh
+++
@@ -1,8 +0,0 @@
-echo 'clean up existing  pods and services '
-kubectl delete -f ../busybox.yaml
-
-kubectl delete -f ../fluentd-daemonset.yaml
-
-kubectl delete -f ../kibana.yml
-
-kubectl delete -f ../elastic-search.yml
